7ecff5c8b3f9def5034f12218d4ebafd1113b614
chenjiabin <80477286@163.com>
2017-08-23 10:29:35 +0800

    Initial commit

---
+++ wireless-requirements-management-platform/src/main/java/com/huawei/wireless/wrmp/WrmpApplication.java
@@ -0,0 +1,54 @@
+package com.huawei.wireless.wrmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by cwx183898 on 2017/8/8.
+ */
+@SpringBootApplication
+@Controller
+public class WrmpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WrmpApplication.class, args);
+    }
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "/index";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/error/403")
+    public String error403() {
+        return "/error/403";
+    }
+}
---
+++ wireless-requirements-management-platform/src/main/java/com/huawei/wireless/wrmp/authorization/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.huawei.wireless.wrmp.authorization;
+
+import com.mouse.web.authorization.ldap.WebLdapSecurityConfigurerAdapter;
+import com.mouse.web.authorization.ll.WebLdapAndLocalSecurityConfigurerAdapter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+/**
+ * Created by cwx183898 on 2017/8/9.
+ */
+@Configuration
+public class SecurityConfiguration extends WebLdapAndLocalSecurityConfigurerAdapter {
+    private static final Log LOGGER = LogFactory.getLog(SecurityConfiguration.class);
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    public String getPermits() {
+        return "/,/about,/index,/index.html,/**/*.css,/**/*.js,/**/*.gif,/**/*.jpg,/**/*.bmp,/**/*.png,/**/*.ico";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String permits = this.getPermits();
+        String[] matchers = StringUtils.isEmpty(permits) ? new String[0] : getPermits().split("[,]");
+        LOGGER.info("自定义免验证地址列表：" + Arrays.toString(matchers));
+        http.authenticationProvider(getAuthenticationProvider()).addFilterBefore(filter, FilterSecurityInterceptor.class)
+                .authorizeRequests()
+                .antMatchers(matchers).permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll()
+                .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+                .and().csrf().disable();
+    }
+}
