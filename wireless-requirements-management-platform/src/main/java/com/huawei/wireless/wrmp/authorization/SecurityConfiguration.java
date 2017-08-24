e7c787aec6a74a7ac9aa35a2b58b780766d67f45
chenjiabin <80477286@163.com>
2017-08-24 09:39:19 +0800

    init

--- wireless-requirements-management-platform/src/main/java/com/huawei/wireless/wrmp/authorization/SecurityConfiguration.java
+++ wireless-requirements-management-platform/src/main/java/com/huawei/wireless/wrmp/authorization/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.huawei.wireless.wrmp.authorization;
 import com.huawei.wireless.wrmp.authorization.handlers.MyAccessDeniedHandler;
 import com.mouse.web.authorization.ldap.WebLdapSecurityConfigurerAdapter;
 import com.mouse.web.authorization.ll.WebLdapAndLocalSecurityConfigurerAdapter;
+import com.mouse.web.authorization.local.WebLocalSecurityConfigurerAdapter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -18,7 +20,7 @@ import java.util.Arrays;
  * Created by cwx183898 on 2017/8/9.
  */
 @Configuration
-public class SecurityConfiguration extends WebLdapAndLocalSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebLocalSecurityConfigurerAdapter {
     private static final Log LOGGER = LogFactory.getLog(SecurityConfiguration.class);
     @Autowired
     private MyAccessDeniedHandler accessDeniedHandler;
@@ -42,4 +44,5 @@ public class SecurityConfiguration extends WebLdapAndLocalSecurityConfigurerAdap
                 .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler)
                 .and().csrf().disable();
     }
+
 }
