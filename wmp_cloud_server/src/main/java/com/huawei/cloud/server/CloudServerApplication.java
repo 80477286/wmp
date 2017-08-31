8c22e3d35b4bd2a0cab832ace5e47035fbe5a421
chenjiabin <80477286@163.com>
2017-08-31 17:49:53 +0800

    init

---
+++ wmp_cloud_server/src/main/java/com/huawei/cloud/server/CloudServerApplication.java
@@ -0,0 +1,16 @@
+package com.huawei.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by cwx183898 on 2017/5/19.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerApplication.class, args);
+    }
+}
