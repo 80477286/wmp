7335fd8772783c816f1031218984e2b6b3eb6e26
chenjiabin <80477286@163.com>
2017-09-05 10:19:35 +0800

    init

--- wireless-requirements-management-platform/src/main/java/com/chinasoft/wireless/wrmp/WrmpApplication.java
+++ wireless-requirements-management-platform/src/main/java/com/chinasoft/wireless/wrmp/WrmpApplication.java
@@ -1,7 +1,12 @@
 package com.chinasoft.wireless.wrmp;
 
+import com.mouse.web.supports.jpa.repository.RepositoryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,6 +17,8 @@ import java.security.Principal;
  */
 @SpringBootApplication
 @Controller
+@ComponentScan(basePackages = {"com.mouse"})
+@EnableJpaRepositories(repositoryFactoryBeanClass = RepositoryFactory.class)
 public class WrmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(WrmpApplication.class, args);
@@ -20,7 +27,10 @@ public class WrmpApplication {
     @GetMapping({"/", "/index"})
     public String index(Principal principal) {
         if (principal != null) {
-            System.out.println(principal.getName());
+            if (principal instanceof OAuth2Authentication) {
+                OAuth2Authentication oa = (OAuth2Authentication) principal;
+                System.out.println(((OAuth2AuthenticationDetails) oa.getDetails()).getTokenValue());
+            }
         }
         return "/index";
     }
