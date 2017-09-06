91effcc09bd64137df8f71ca5a917043676c9bf3
chiliao2 <553186538@qq.com>
2017-09-06 11:13:38 +0800

    删除掉feignClient模式

--- wireless-measurement-platform/src/main/java/com/chinasoft/WmpApplication.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/WmpApplication.java
@@ -4,9 +4,7 @@ import com.mouse.web.supports.jpa.repository.RepositoryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
@@ -18,12 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @EnableEurekaClient
 @SpringBootApplication
 @Controller
-@EnableFeignClients
-@EnableHystrix
-@EnableHystrixDashboard
 @ComponentScan(basePackages = {"com.mouse", "com.chinasoft"})
 @EnableJpaRepositories(repositoryFactoryBeanClass = RepositoryFactory.class)
-
 public class WmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(WmpApplication.class, args);
