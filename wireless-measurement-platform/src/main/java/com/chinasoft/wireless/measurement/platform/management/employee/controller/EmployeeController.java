7b38be4b977498bcf8b69f3fa727d60c87c00863
chiliao2 <553186538@qq.com>
2017-09-06 11:30:18 +0800

    分页测试

--- wireless-measurement-platform/src/main/java/com/chinasoft/wireless/measurement/platform/management/employee/controller/EmployeeController.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/wireless/measurement/platform/management/employee/controller/EmployeeController.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +27,7 @@ public class EmployeeController {
     DiscoveryClient client;
 
     @RequestMapping(value = "/employees", method = RequestMethod.POST)
-    public Object getAllEmployees(@RequestParam Map<String, Object> params) {
+    public Object getAllEmployees(@RequestParam LinkedMultiValueMap<String, Object> params) {
         List<ServiceInstance> instances = client.getInstances("hrm-service");
         if (instances != null && !instances.isEmpty()) {
             OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
@@ -34,6 +36,7 @@ public class EmployeeController {
             ServiceInstance instance = instances.get(0);
             URI uri = instance.getUri();
             HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             httpHeaders.add("Authorization", type + " " + token);
             HttpEntity entity = new HttpEntity(params, httpHeaders);
             return (new RestTemplate()).postForEntity(uri.toString() + "/hrm/employee/query", entity, Map.class).getBody();
