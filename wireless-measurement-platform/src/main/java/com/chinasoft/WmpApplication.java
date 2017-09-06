3934fd88e1ad1fc857a8e1f7b5414e5004c1d4f6
chenjiabin <80477286@163.com>
2017-09-06 11:22:03 +0800

    init

--- wireless-measurement-platform/src/main/java/com/chinasoft/WmpApplication.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/WmpApplication.java
@@ -3,34 +3,29 @@ package com.chinasoft;
 import com.mouse.web.supports.jpa.repository.RepositoryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 /**
  *
  */
 @EnableEurekaClient
 @SpringBootApplication
-@Controller
-@EnableFeignClients
-@EnableHystrix
-@EnableHystrixDashboard
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.mouse", "com.chinasoft"})
 @EnableJpaRepositories(repositoryFactoryBeanClass = RepositoryFactory.class)
-
 public class WmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(WmpApplication.class, args);
     }
 
     @GetMapping({"/index", "/"})
-    public String login() {
+    public String index(Principal principal) {
         return "/index";
     }
 }
