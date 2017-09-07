4c7e9ed2de781a8522bb60973c4eb75f76a465ca
chenjiabin <80477286@163.com>
2017-09-07 10:50:26 +0800

    init

---
+++ wireless-measurement-platform/src/main/java/com/chinasoft/GlobalController.java
@@ -0,0 +1,20 @@
+package com.chinasoft;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class GlobalController {
+    @GetMapping({"/index", "/"})
+    public String index(Principal principal) {
+        return "/index";
+    }
+}
