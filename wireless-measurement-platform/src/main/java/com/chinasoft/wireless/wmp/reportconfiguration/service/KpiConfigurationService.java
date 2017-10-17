09853c7635ebbc2bd40ab66075530a2d4d7d7c11
chiliao2 <553186538@qq.com>
2017-10-17 17:04:09 +0800

    增加指标和报表实体

--- wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/service/KpiConfigurationService.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/service/KpiConfigurationService.java
@@ -4,7 +4,6 @@ import com.chinasoft.wireless.wmp.reportconfiguration.model.KpiConfiguration;
 import com.chinasoft.wireless.wmp.reportconfiguration.repository.KpiConfigurationRepository;
 import com.mouse.web.supports.jpa.repository.BaseRepository;
 import com.mouse.web.supports.jpa.service.BaseService;
-import com.mouse.web.supports.jpa.service.IBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
