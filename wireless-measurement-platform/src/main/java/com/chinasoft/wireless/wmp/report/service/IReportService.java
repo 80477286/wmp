a76014c6c8d21b755927e1eed1008512c5355782
chenjiabin <80477286@163.com>
2017-10-18 14:35:19 +0800

    调整项目结构

--- wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/report/service/IReportService.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/report/service/IReportService.java
@@ -2,6 +2,12 @@ package com.chinasoft.wireless.wmp.report.service;
 
 import com.chinasoft.wireless.wmp.report.model.Report;
 import com.mouse.web.supports.jpa.service.IBaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Map;
 
 public interface IReportService extends IBaseService<Report, String> {
+
+    Map<String, Object> queryReport(final Map<String, Object> params, final Pageable pageable);
 }
