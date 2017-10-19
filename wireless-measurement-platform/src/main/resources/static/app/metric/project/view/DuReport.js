77e8c108a6ebeda0fbb463887daf9cfad2d93403
chenjiabin <80477286@163.com>
2017-10-19 11:34:39 +0800

    调整项目结构

--- wireless-measurement-platform/src/main/resources/static/app/metric/project/view/DuReport.js
+++ wireless-measurement-platform/src/main/resources/static/app/metric/project/view/DuReport.js
@@ -15,5 +15,33 @@ Ext.define('App.metric.project.view.DuReport', {
                     // });
                 }
             },
+        },
+        {
+            xtype: 'Report',
+            title: '软件开发',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    // me.load('report/queryReport', {
+                    //     'params.projectId': me.up().node.data.id,
+                    //     'params.reportConfigurationType': '迭代度量',
+                    //     'params.groupName': '需求'
+                    // });
+                }
+            },
+        },
+        {
+            xtype: 'Report',
+            title: '硬件测试',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    // me.load('report/queryReport', {
+                    //     'params.projectId': me.up().node.data.id,
+                    //     'params.reportConfigurationType': '迭代度量',
+                    //     'params.groupName': '需求'
+                    // });
+                }
+            },
         }]
 })
