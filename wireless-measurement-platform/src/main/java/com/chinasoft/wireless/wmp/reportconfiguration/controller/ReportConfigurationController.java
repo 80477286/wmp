2ea9c9758d9cc2c735eef5c292033ff63b8dd0fa
chiliao2 <553186538@qq.com>
2017-10-23 15:12:08 +0800

    报表配置修改与添加

--- wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/controller/ReportConfigurationController.java
+++ wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/controller/ReportConfigurationController.java
@@ -4,6 +4,8 @@ import com.chinasoft.wireless.wmp.reportconfiguration.model.ReportConfiguration;
 import com.chinasoft.wireless.wmp.reportconfiguration.service.IReportConfigurationService;
 import com.mouse.web.supports.jpa.controller.BaseController;
 import com.mouse.web.supports.jpa.service.IBaseService;
+import com.mouse.web.supports.mvc.bind.annotation.EntityParam;
+import com.mouse.web.supports.mvc.bind.annotation.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,13 @@ public class ReportConfigurationController extends BaseController<ReportConfigur
     @Override
     protected IBaseService<ReportConfiguration, String> getService() {
         return reportConfigurationService;
+    }
+
+    @Override
+    @JSON()
+    @RequestMapping(value = "/save")
+    public ReportConfiguration save(@EntityParam ReportConfiguration reportConfiguration) {
+        ReportConfiguration result = getService().save(reportConfiguration);
+        return result;
     }
 }
