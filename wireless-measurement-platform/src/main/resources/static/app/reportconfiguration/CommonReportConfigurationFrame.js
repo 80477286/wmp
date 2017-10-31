71f6aee69d815ac1c32b95b7de264aa79897987e
chiliao2 <553186538@qq.com>
2017-10-31 10:18:53 +0800

    报表配置修改与添加

--- wireless-measurement-platform/src/main/resources/static/app/reportconfiguration/CommonReportConfigurationFrame.js
+++ wireless-measurement-platform/src/main/resources/static/app/reportconfiguration/CommonReportConfigurationFrame.js
@@ -7,6 +7,25 @@ Ext.define('App.reportconfiguration.CommonReportConfigurationFrame', {
         {
             xtype: 'ReportConfigurationForm',
             title: 'BU报表配置 ',
+            tbar: [{
+                xtype: 'button',
+                text: '保存',
+                handler: function () {
+                    var me = this;
+                    var form = me.up('ReportConfigurationForm');
+                    form.submit({
+                        url: '/report_configuration/save',
+                        method: 'POST',
+                        success: function () {
+                            Ext.MessageBox.alert('提示', '保存成功');
+                        },
+                        failure: function () {
+                            Ext.MessageBox.alert('提示', '保存失败');
+                        }
+                    });
+
+                }
+            }],
             listeners: {
                 afterrender: function () {
                     this.load({url: '/report_configuration/get_by_id', params: {id: 1}});
