d9328068f1c5a388a97c52c57803e044b4a8be0b
chenjiabin <80477286@163.com>
2017-11-02 19:51:32 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/controller/ReportConfigurationController.java
    #	wireless-measurement-platform/src/main/resources/static/management/app/reportconfiguration/view/ReportConfigurationWindow.js

--- wireless-measurement-platform/src/main/resources/static/app/metric/project/view/IterationReport.js
+++ wireless-measurement-platform/src/main/resources/static/app/metric/project/view/IterationReport.js
@@ -3,25 +3,133 @@ Ext.define('App.metric.project.view.IterationReport', {
     alias: ['widget.IterationReport'],
     items: [
         {
-            xtype: 'Report',
             title: '需求',
+            xtype: 'Report',
             listeners: {
                 afterrender: function () {
                     var me = this;
-                    me.load('report/queryReport', {
-                        'params.project.id': me.up().node.data.id,
-                        'params.reportConfiguration.type': '迭代度量-需求'
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-需求'
                     });
                 }
-            },
+            }
         },
-        {title: '设计'},
-        {title: '开发'},
-        {title: '测试'},
-        {title: '构建'},
-        {title: '转测试'},
-        {title: '迭代管理'},
-        {title: '工程能力'},
-        {title: '验收'},
-        {title: '客户反馈'}]
+        {
+            title: '设计',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-设计'
+                    });
+                }
+            }
+        },
+        {
+            title: '开发',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-开发'
+                    });
+                }
+            }
+        },
+        {
+            title: '测试',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-测试'
+                    });
+                }
+            }
+        },
+        {
+            title: '构建',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-构建'
+                    });
+                }
+            }
+        },
+        {
+            title: '转测试',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-转测试'
+                    });
+                }
+            }
+        },
+        {
+            title: '迭代管理',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-迭代管理'
+                    });
+                }
+            }
+        },
+        {
+            title: '工程能力',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-工程能力'
+                    });
+                }
+            }
+        },
+        {
+            title: '验收',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-验收'
+                    });
+                }
+            }
+        },
+        {
+            title: '客户反馈',
+            xtype: 'Report',
+            listeners: {
+                afterrender: function () {
+                    var me = this;
+                    me.load('report/query_iteration_report', {
+                        'projectId': me.up().node.data.id,
+                        'reportConfigurationType': '迭代度量-客户反馈'
+                    });
+                }
+            }
+        }]
 })
