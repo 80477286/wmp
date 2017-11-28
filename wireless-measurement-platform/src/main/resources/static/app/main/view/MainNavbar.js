95c7c1e330f83ce36a05317c674b316c29d5f8a1
cwx183898 <80477286@163.com>
2017-11-28 15:11:04 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	wireless-measurement-platform/src/main/java/com/chinasoft/wireless/wmp/reportconfiguration/controller/ReportConfigurationController.java
    #	wireless-measurement-platform/src/main/resources/static/management/app/reportconfiguration/view/ReportConfigurationWindow.js

--- wireless-measurement-platform/src/main/resources/static/app/main/view/MainNavbar.js
+++ wireless-measurement-platform/src/main/resources/static/app/main/view/MainNavbar.js
@@ -30,5 +30,55 @@ Ext.define('App.main.view.MainNavbar', {
             href: '/management.html',
             target: '_blank'
         }
-    ]
+    ], listeners: {
+        afterrender: function () {
+            this.createLeftButtons();
+        }
+    },
+    createLeftButtons: function () {
+        var spec = {
+            id: 'buttons-ul',
+            tag: 'ul',
+            cls: 'buttons-ul',
+            // append children after creating
+            children: [
+                {
+                    tag: 'li', cls: 'po-config', title: '项目订单',
+                    children: [
+                        {tag: 'a', cls: 'icon', href: "/?po", target: "_blank"}
+                    ]
+                },
+                {
+                    tag: 'li', cls: 'qa-config', title: '项目质量',
+                    children: [
+                        {tag: 'a', cls: 'icon', href: "/?qa", target: "_blank"}
+                    ]
+                },
+                {
+                    tag: 'li', cls: 'pm-config', title: '项目管理',
+                    children: [
+                        {tag: 'a', cls: 'icon', href: "/?pm", target: "_blank"}
+                    ]
+                },
+                {
+                    tag: 'li', cls: 'admin-config', title: '系统配置',
+                    children: [
+                        {tag: 'a', cls: 'icon', href: "/?administrator", target: "_blank"}
+                    ]
+                },
+                {
+                    tag: 'li', cls: 'logout', title: '注销',
+                    children: [
+                        {tag: 'a', cls: 'icon', href: "/logout", target: "_blank"}
+                    ]
+                }
+            ]
+        };
+        var buttons = Ext.dom.Helper.append(
+            this.body, // the context element 'my-div' can either be the id or the actual node
+            spec,
+            true    // the specification object
+        )
+    }
+
 })
