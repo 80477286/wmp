310d557976af6a51d27b7649f16e1e4eddc64ee8
chiliao2 <553186538@qq.com>
2017-11-30 10:08:04 +0800

    报表配置优化

--- wireless-measurement-platform/src/main/resources/static/app/metric/project/view/ProjectOrderReport.js
+++ wireless-measurement-platform/src/main/resources/static/app/metric/project/view/ProjectOrderReport.js
@@ -1,9 +1,40 @@
 Ext.define('App.metric.project.view.ProjectOrderReport', {
     extend: 'Ext.tab.Panel',
     alias: ['widget.ProjectOrderReport'],
+    requires: ['App.projectorder.view.ProjectOrderFormPanel'],
     items: [{
-        title:'PO信息',
-        xtype:'panel'
+        title: 'PO信息',
+        xtype: 'panel',
+        items: [{
+            xtype: 'ProjectOrderFormPanel'
+        }],
+        listeners: {
+            afterrender: function () {
+                var me = this;
+                var formpanel = me.down('ProjectOrderFormPanel');
+                formpanel.load({
+                    url: 'projectorder/edit',
+                    params: {
+                        'id': me.up().node.data.id
+                    },
+                    success: function (form, result, data) {
+                        formpanel.down('[name=parent.name]').setValue(result.result.data.parent.name);
+                        var startTime = result.result.data.startTime;
+                        var planStartTime = result.result.data.planStartTime;
+                        var endTime = result.result.data.endTime;
+                        if (startTime != null) {
+                            formpanel.down('[name=startTime]').setValue(substringTime(startTime));
+                        }
+                        if (planStartTime != null) {
+                            formpanel.down('[name=planStartTime]').setValue(substringTime(planStartTime));
+                        }
+                        if (endTime != null) {
+                            formpanel.down('[name=endTime]').setValue(substringTime(endTime));
+                        }
+                    }
+                });
+            }
+        }
     },
         {
             xtype: 'Report',
@@ -19,3 +50,7 @@ Ext.define('App.metric.project.view.ProjectOrderReport', {
             },
         }]
 })
+
+function substringTime(time) {
+    return time.substring(0, time.indexOf('T'));
+}
